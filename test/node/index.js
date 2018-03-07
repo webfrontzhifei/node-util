a64e663cb2300bcd186bbe40b760c3d40ed37793
Renée Kooi <renee@kooi.me>
2018-03-07 16:11:17 +0100

    add callbackify

--- test/node/index.js
+++ test/node/index.js
@@ -5,3 +5,4 @@ spawn(process.argv[0], [ require.resolve('./format') ], { stdio: 'inherit' });
 spawn(process.argv[0], [ require.resolve('./inspect') ], { stdio: 'inherit' });
 spawn(process.argv[0], [ require.resolve('./log') ], { stdio: 'inherit' });
 spawn(process.argv[0], [ require.resolve('./promisify') ], { stdio: 'inherit' });
+spawn(process.argv[0], [ require.resolve('./callbackify') ], { stdio: 'inherit' });
